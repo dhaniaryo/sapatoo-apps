b591dcc58e009edc0546f01a0961dbdeb7e3d355
dhaniaryo <dhanipriyono01@gmail.com>
2021-07-30 18:45:20 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sapatoo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sapatoo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sapatoo/pages/signin_page.dart';
+import 'package:sapatoo/pages/splash_page.dart';
+// import 'theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context)=> SplashPage(),
+        '/signin': (context)=>SignInPage(),
+      },
+    );
+  }
+}
