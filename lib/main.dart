0b81dfa3fe4a593ae2a797291c3b134bab464c29
dhaniaryo <dhanipriyono01@gmail.com>
2021-07-30 19:03:27 +0700

    adding signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sapatoo/pages/signin_page.dart';
+import 'package:sapatoo/pages/signup_page.dart';
 import 'package:sapatoo/pages/splash_page.dart';
 // import 'theme.dart';
 
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context)=> SplashPage(),
         '/signin': (context)=>SignInPage(),
+        '/signup': (context)=>SignUpPage(),
       },
     );
   }
