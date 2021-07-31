73ce7101932324d6291d0d7f93853a5ebc30828a
dhaniaryo <dhanipriyono01@gmail.com>
2021-07-31 12:20:28 +0700

    adding homepage and content

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,9 +12,12 @@ Color backgroundColor1 = Color(0xff1F1D2B);
 Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color backgroundColor4 = Color(0xff252836);
+Color backgroundColor5 = Color(0xffECEDEF);
+Color transparentColor = Colors.transparent;
 Color primaryTextColor = Color(0xffE1E1E1);
 Color secondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504F5E);
+Color blackColor = Color(0xff2E2E2E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -34,6 +37,10 @@ TextStyle priceTextStyle = GoogleFonts.poppins(
 
 TextStyle purpleTextStyle = GoogleFonts.poppins(
   color: primaryColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
 );
 
 FontWeight light = FontWeight.w300;
